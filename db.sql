212865cad395e74852d756f32482129f16b3cd6d
Moxie <ysmarina1234@gmail.com>
2024-05-17 23:27:30 +0530

    initial database created

---
+++ db.sql
@@ -0,0 +1,64 @@
+-- Create table for users
+CREATE TABLE users (
+    user_id SERIAL PRIMARY KEY,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+		first_name VARCHAR(100) NOT NULL,
+		last_name VARCHAR(100),
+    password VARCHAR(255) NOT NULL,
+);
+
+-- Create table for student_users
+CREATE TABLE student_users (
+    user_id INT PRIMARY KEY REFERENCES users(user_id) ON DELETE CASCADE,
+    dob DATE
+);
+
+-- Create table for teacherusers
+CREATE TABLE teacher_users (
+    user_id INT PRIMARY KEY REFERENCES users(user_id) ON DELETE CASCADE,
+    department VARCHAR(100)
+);
+
+-- Create table for posts
+CREATE TABLE posts (
+    post_id SERIAL PRIMARY KEY,
+    user_id INT REFERENCES users(user_id) ON DELETE CASCADE,
+    content TEXT,
+    media_type VARCHAR(50),
+    media_url VARCHAR(255),
+    teacher_verified BOOLEAN,
+    timestamp TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Create table for comments
+CREATE TABLE comments (
+    comment_id SERIAL PRIMARY KEY,
+    post_id INT REFERENCES posts(post_id) ON DELETE CASCADE,
+    user_id INT REFERENCES users(user_id) ON DELETE CASCADE,
+    content TEXT,
+    timestamp TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Create table for likes
+CREATE TABLE likes (
+    like_id SERIAL PRIMARY KEY,
+    post_id INT REFERENCES posts(post_id) ON DELETE CASCADE,
+    comment_id INT REFERENCES comments(comment_id) ON DELETE CASCADE,
+    user_id INT REFERENCES users (user_id) ON DELETE CASCADE,
+    timestamp TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Create table for messages
+CREATE TABLE messages (
+    message_id SERIAL PRIMARY KEY,
+    sender_id INT REFERENCES users(user_id) ON DELETE CASCADE,
+    receiver_id INT REFERENCES users(user_id) ON DELETE CASCADE,
+    content TEXT,
+    timestamp TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
+);
+
+
+-- Ensure that a user can only like a post once
+ALTER TABLE likes
+ADD CONSTRAINT unique_user_post_like UNIQUE (user_id, post_id);
