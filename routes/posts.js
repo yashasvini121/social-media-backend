0c5b3c3b34a34c3778e83d92437b1e91d38ec58d
Moxie <ysmarina1234@gmail.com>
2024-05-19 09:12:15 +0530

    updating posts routes with auth

--- routes/posts.js
+++ routes/posts.js
@@ -1,12 +1,22 @@
 const express = require("express");
+const authMiddleware = require("../middlewares/authMiddleware");
 const router = express.Router();
+
 const {
   getPosts,
   createPost,
   updatePostById,
+  deletePostById,
 } = require("../controllers/postsController");
 
-router.route("/").get(getPosts).post(createPost);
-router.route("/:id").patch(updatePostById);
+router
+  .route("/")
+  .get(authMiddleware, getPosts)
+  .post(authMiddleware, createPost);
+
+router
+  .route("/:id")
+  .patch(authMiddleware, updatePostById)
+  .delete(authMiddleware, deletePostById);
 
 module.exports = router;
