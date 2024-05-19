46d12c6bd23bc2c47d43ad9bb0066b7f04b3f100
Moxie <ysmarina1234@gmail.com>
2024-05-19 09:25:31 +0530

    updating comments routes with auth

--- routes/comments.js
+++ routes/comments.js
@@ -1,5 +1,7 @@
 const express = require("express");
+const authMiddleware = require("../middlewares/authMiddleware");
 const router = express.Router();
+
 const {
   getComments,
   getCommentsByPostId,
@@ -8,8 +10,16 @@ const {
   deleteComment,
 } = require("../controllers/commentsController");
 
-router.route("/").get(getComments);
-router.route("/post/:post_id/").get(getCommentsByPostId).post(createComment);
-router.route("/:comment_id").patch(updateComment).delete(deleteComment);
+router.route("/").get(authMiddleware, getComments);
+
+router
+  .route("/post/:post_id/")
+  .get(authMiddleware, getCommentsByPostId)
+  .post(authMiddleware, createComment);
+
+router
+  .route("/:comment_id")
+  .patch(authMiddleware, updateComment)
+  .delete(authMiddleware, deleteComment);
 
 module.exports = router;
