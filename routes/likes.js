0bf20571ca4efca1e8ade486fea0eec6b565710a
Moxie <ysmarina1234@gmail.com>
2024-05-19 09:25:18 +0530

    updating likes routes with auth

--- routes/likes.js
+++ routes/likes.js
@@ -1,10 +1,15 @@
 const express = require("express");
+const authMiddleware = require("../middlewares/authMiddleware");
 const router = express.Router();
+
 const {
   like,
   unlike,
 } = require("../controllers/likesController");
 
-router.route("/").post(like).delete(unlike);
+router
+  .route("/")
+  .post(authMiddleware, like)
+  .delete(authMiddleware, unlike);
 
 module.exports = router;
