a87357ba60f8971c5d92eeea0b7b17f49211b844
Moxie <ysmarina1234@gmail.com>
2024-05-19 09:35:14 +0530

    using using basic model in user controllers

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,13 +1,22 @@
 const pool = require("../config/db");
+const User = require("../models/User");
 
 exports.getUsers = async (req, res) => {
-  try {
-    const result = await pool.query("SELECT * FROM users");
-    res.status(200).json(result.rows);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Internal Server Error" });
-  }
+  // try {
+  //   const result = await pool.query("SELECT * FROM users");
+  //   res.status(200).json(result.rows);
+  // } catch (error) {
+  //   console.error(error);
+  //   res.status(500).json({ error: "Internal Server Error" });
+  // }
+
+    try {
+      const users = await User.getAll();
+      res.status(200).json(users);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: "Internal Server Error" });
+    }
 };
 
 exports.createUser = async (req, res) => {
